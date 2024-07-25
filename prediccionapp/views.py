d8a85faf986fb0097c581c982aa2f86b1ac9db02
Mishaelrv <mijharv@gmail.com>
2024-07-24 21:49:58 -0500

    Prediccion 01 terminada

--- prediccionapp/views.py
+++ prediccionapp/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from rest_framework.views import APIView 
+from rest_framework.response import Response 
+from rest_framework import status
+from .models import predict
+from .serializers import PredictionSerializer
+import numpy as np
 
-# Create your views here.
+class PredictView(APIView):
+    def post(self, request, *args, **kwargs):
+        serializer = PredictionSerializer(data=request.data)
+        if serializer.is_valid():
+            data = serializer.validated_data
+            features = [
+                data['Genero'],
+                data['Edad'],
+                data['TipoTrabajo'],
+                data['Hipertension'],
+                data['Cardiopatia'],
+                data['Nivel_GlucosaPromedio'],
+                data['ICM'],
+                data['EstadoFumador']
+            ]
+            try:
+                # Realizar la predicción
+                prediction = predict(features)
+                
+                #features_array = np.array([features])
+                return Response({'prediction': prediction.tolist()})
+            except Exception as e:
+                return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+def prediction_form(request):
+    return render(request, 'form-acv1.html')
--- proymodelopredictivo/urls.py
+++ proymodelopredictivo/urls.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 from django.urls import path
 from seguridadapp.views import views, usuario
 from django.urls import path,include
+from prediccionapp.views import PredictView, prediction_form
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -14,4 +16,7 @@ urlpatterns = [
     # Usuarios
     path('usuarios/',include('seguridadapp.routes.usuario'),name="usuarios"),
     path('update-status-user/',usuario.update_user_status,name="update-status-user"),
+
+    path('api/acv1', PredictView.as_view(),name="acv1"),    
+    path('prediccion/', prediction_form, name='prediction_form'),
 ]
